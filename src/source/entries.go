0d3740e40b88879b84a1c8467a9e0f2491d249a2
chilarai <chilly5476@gmail.com>
2021-11-16 01:54:30 +0530

    Quizzes

--- src/source/entries.go
+++ src/source/entries.go
@@ -4,19 +4,23 @@ import (
 	"common"
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
 )
 
 // EntriesOutputStruct return
 type EntriesOutputStruct struct {
 	StatusOut   common.Status     `json:"status"`
+	QuizCategory string `json:"category"`
+	QuizSubCateogry string `json:"sub-category"`
 	ListDataOut []*EntriesOutputData `json:"data"`
 }
 
 // EntriesOutputData struct
 type EntriesOutputData struct {
-	CategoryID            int
-	CategoryName          string
+	RightOption      string
+	Options          []string
+	ImageLink		 string
 }
 
 func QuizEntries(w http.ResponseWriter, req *http.Request){
@@ -34,12 +38,15 @@ func QuizEntries(w http.ResponseWriter, req *http.Request){
 	}
 
 	var inputJSON input
-	var CategoryName string
-	var CategoryID int
+	var Entry, ImageLink, Category, SubCategory string
 
 	var status common.Status
-	var outputStruct CategoriesOutputStruct
-	var result []*CategoriesOutputData
+	var outputStruct EntriesOutputStruct
+	var result []*EntriesOutputData
+	var answers []string
+	var answersMap =  make(map[string]string)
+	var answerOptions []string
+	
 
 	if(inputJSON.SessionKey == ""){
 		status = common.Status{
@@ -47,7 +54,7 @@ func QuizEntries(w http.ResponseWriter, req *http.Request){
 			Message: common.InvalidInput,
 		}
 	} else {
-		selCategories, errSel := mydb.Query("SELECT * FROM categories")
+		selEntries, errSel := mydb.Query("SELECT entry, image_link FROM entries WHERE quiz_id = ? AND quizsubcategory_id = ?", inputJSON.CategoryID, inputJSON.SubCategoryID)
 
 		if(errSel != nil){
 
@@ -57,20 +64,52 @@ func QuizEntries(w http.ResponseWriter, req *http.Request){
 			}
 
 		} else{
-			for selCategories.Next(){
-				errSelScan := selCategories.Scan(&CategoryID, &CategoryName)
+			for selEntries.Next(){
+				errSelScan := selEntries.Scan(&Entry, &ImageLink)
 
 				if errSelScan != nil {
 					log.Println(errSelScan)
 				} else {
 
-					elem := CategoriesOutputData{
-						CategoryID: CategoryID,
-						CategoryName: CategoryName,
-					}
-
-					result = append(result, &elem)
+					answers = append(answers, Entry)
+					answersMap[Entry] = ImageLink
 				}
+			}
+
+			answersLength := len(answers)
+			for ans, image := range answersMap {
+
+				answerOptions = nil
+
+				if(answersLength >= 4){
+					for len(answerOptions) <= 4 {
+						CreateAnswerOptions(answers, &answerOptions)
+					}
+				} else if (answersLength >= 3) {
+
+					for len(answerOptions) <= 3 {
+						CreateAnswerOptions(answers, &answerOptions)
+					}
+				} else {
+
+					for len(answerOptions) <= 2 {
+						CreateAnswerOptions(answers, &answerOptions)
+					}
+				}
+
+				elem := EntriesOutputData{
+					RightOption : ans,
+					Options : answerOptions,
+					ImageLink : image,
+				}
+
+				result = append(result, &elem)
+			}
+
+			errSelQuiz := mydb.QueryRow("SELECT quizsubcategories.subcategory_name, quizes.quiz_name FROM quizsubcategories LEFT JOIN quizes ON quizes.id = quizsubcategories.quiz_id WHERE quizsubcategories.id = ?", inputJSON.SubCategoryID).Scan(&SubCategory, &Category)
+
+			if errSelQuiz != nil{
+				log.Println(errSelQuiz)
 			}
 
 			status = common.Status{
@@ -80,12 +119,35 @@ func QuizEntries(w http.ResponseWriter, req *http.Request){
 		}
 	}
 
-	outputStruct = CategoriesOutputStruct{
+	outputStruct = EntriesOutputStruct{
 		StatusOut: status,
+		QuizCategory: Category,
+		QuizSubCateogry: SubCategory,
 		ListDataOut: result,
 	}
 
 	output, _ := json.Marshal(outputStruct)
 
 	w.Write(output)
+}
+
+
+func Find(slice []string, val string) (int, bool) {
+    for i, item := range slice {
+        if item == val {
+            return i, true
+        }
+    }
+    return -1, false
+}
+
+func CreateAnswerOptions(answers []string, answerOptions *[]string){
+	randomIndex := rand.Intn(len(answers))
+	pick := answers[randomIndex]
+
+	_, matches := Find(*answerOptions, pick)
+
+	if !matches{
+		*answerOptions = append(*answerOptions, pick)
+	}
 }
