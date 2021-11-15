3482a96e75ff204f8b5dcadd7d064111f80d80b1
chilarai <chilly5476@gmail.com>
2021-11-15 16:49:33 +0530

    Categories and sub categories

--- src/source/categories.go
+++ src/source/categories.go
@@ -2,8 +2,22 @@ package source
 
 import (
 	"config"
+	"encoding/json"
+	"log"
 	"net/http"
 )
+
+// CategoriesOutputStruct return
+type CategoriesOutputStruct struct {
+	StatusOut   config.Status     `json:"status"`
+	ListDataOut []*CategoriesOutputData `json:"data"`
+}
+
+// CategoriesOutputData struct
+type CategoriesOutputData struct {
+	CategoryID            int
+	CategoryName          string
+}
 
 func QuizCategories(w http.ResponseWriter, req *http.Request){
 	
@@ -14,9 +28,62 @@ func QuizCategories(w http.ResponseWriter, req *http.Request){
 	defer mydb.Close()
 
 	type input struct {
-		Email string
 		SessionKey string
-		Name string
-		PhotoLink string
 	}
+
+	var inputJSON input
+	var CategoryName string
+	var CategoryID int
+
+	var status config.Status
+	var outputStruct CategoriesOutputStruct
+	var result []*CategoriesOutputData
+
+	if(inputJSON.SessionKey == ""){
+		status = config.Status{
+			Code:    403,
+			Message: config.InvalidInput,
+		}
+	} else {
+		selCategories, errSel := mydb.Query("SELECT * FROM categories")
+
+		if(errSel != nil){
+
+			status = config.Status{
+				Code:    403,
+				Message: errSel.Error(),
+			}
+
+		} else{
+			for selCategories.Next(){
+				errSelScan := selCategories.Scan(&CategoryID, &CategoryName)
+
+				if errSelScan != nil {
+					log.Println(errSelScan)
+				} else {
+
+					elem := CategoriesOutputData{
+						CategoryID: CategoryID,
+						CategoryName: CategoryName,
+					}
+
+					result = append(result, &elem)
+				}
+			}
+
+			status = config.Status{
+				Code:    200,
+				Message: config.SuccessMsg,
+			}
+		}
+	}
+
+	outputStruct = CategoriesOutputStruct{
+		StatusOut: status,
+		ListDataOut: result,
+	}
+
+	output, _ := json.Marshal(outputStruct)
+
+	w.Write(output)
 }
--- src/source/subcategories.go
+++ src/source/subcategories.go
@@ -1,7 +1,90 @@
 package source
 
-import "net/http"
+import (
+	"config"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// SubCategoriesOutputStruct return
+type SubCategoriesOutputStruct struct {
+	StatusOut   config.Status     `json:"status"`
+	ListDataOut []*SubCategoriesOutputData `json:"data"`
+}
+
+// SubCategoriesOutputData struct
+type SubCategoriesOutputData struct {
+	SubCategoryID            int
+	SubCategoryName          string
+}
 
 func QuizSubCategories(w http.ResponseWriter, req *http.Request){
 	
+	config.Cors(&w)
+	w.Header().Set("Content-Type", "application/json")
+
+	mydb := config.Mysqlconnect()
+	defer mydb.Close()
+
+	type input struct {
+		SessionKey string
+		CategoryID int
+	}
+
+	var inputJSON input
+	var SubCategoryName string
+	var SubCategoryID int
+
+	var status config.Status
+	var outputStruct SubCategoriesOutputStruct
+	var result []*SubCategoriesOutputData
+
+	if(inputJSON.SessionKey == "" || inputJSON.CategoryID == 0){
+		status = config.Status{
+			Code:    403,
+			Message: config.InvalidInput,
+		}
+	} else {
+		selSubCategories, errSel := mydb.Query("SELECT * FROM quizsubcategories WHERE quiz_id = ?", inputJSON.CategoryID)
+
+		if(errSel != nil){
+
+			status = config.Status{
+				Code:    403,
+				Message: errSel.Error(),
+			}
+
+		} else{
+			for selSubCategories.Next(){
+				errSelScan := selSubCategories.Scan(&SubCategoryID, &SubCategoryName)
+
+				if errSelScan != nil {
+					log.Println(errSelScan)
+				} else {
+
+					elem := SubCategoriesOutputData{
+						SubCategoryID: SubCategoryID,
+						SubCategoryName: SubCategoryName,
+					}
+
+					result = append(result, &elem)
+				}
+			}
+
+			status = config.Status{
+				Code:    200,
+				Message: config.SuccessMsg,
+			}
+		}
+	}
+
+	outputStruct = SubCategoriesOutputStruct{
+		StatusOut: status,
+		ListDataOut: result,
+	}
+
+	output, _ := json.Marshal(outputStruct)
+
+	w.Write(output)
 }
